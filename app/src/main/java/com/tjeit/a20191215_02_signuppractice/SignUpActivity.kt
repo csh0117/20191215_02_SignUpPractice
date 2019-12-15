4ccbcdfc879f2827a792180b98436a03f90486f0
csh0117 <tnsgur!053>
2019-12-15 12:24:01 +0900

    EditText 타이핑 이벤트 체험 - 비번 길이에 따라 다른 결과를 출력

--- app/src/main/java/com/tjeit/a20191215_02_signuppractice/SignUpActivity.kt
+++ app/src/main/java/com/tjeit/a20191215_02_signuppractice/SignUpActivity.kt
@@ -1,9 +1,15 @@
 package com.tjeit.a20191215_02_signuppractice
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import androidx.core.widget.addTextChangedListener
+import kotlinx.android.synthetic.main.activity_sign_up.*
+import kotlin.math.log
 
 class SignUpActivity : BaseActivity() {
-    var lastBackTime = 0L
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
@@ -11,10 +17,29 @@ class SignUpActivity : BaseActivity() {
         setupEvent()
     }
     override fun setValue() {
-        lastBackTime = 10L
     }
     override fun setupEvent() {
-        lastBackTime = 20L
+        pwEdt.addTextChangedListener {
+            Log.d("log]입력된 값", it.toString())
+            val inputStr = it.toString()
+            if (inputStr.length == 0) {
+                pwStatusTxt.text = "비밀번호가 입력되지 않았습니다."
+            } else if (inputStr.length < 8) {
+                pwStatusTxt.text = "최소 8자 이상 입력해주세요."
+            } else {
+                pwStatusTxt.text = "사용 가능한 비밀번호입니다."
+            }
+        }
+//        pwEdt.addTextChangedListener(object : TextWatcher{
+//            override fun afterTextChanged(s: Editable?) {
+//                Log.d("log]입력된 값", s.toString())
+//            }
+//            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+//            }
+//            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+//            }
+//
+//        })
     }
 
 
