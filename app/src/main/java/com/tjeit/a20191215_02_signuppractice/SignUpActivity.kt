9b734b727aead900ee51294077e8f15c9fe01a20
csh0117 <tnsgur!053>
2019-12-15 13:39:03 +0900

    날짜 선택 시 이벤트 처리 - 월 을 처리하기가 다소 곤란 (0~11로 사용하니까)

--- app/src/main/java/com/tjeit/a20191215_02_signuppractice/SignUpActivity.kt
+++ app/src/main/java/com/tjeit/a20191215_02_signuppractice/SignUpActivity.kt
@@ -1,13 +1,16 @@
 package com.tjeit.a20191215_02_signuppractice
 
+import android.app.DatePickerDialog
 import android.graphics.Color
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
+import android.widget.DatePicker
 import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
 import kotlinx.android.synthetic.main.activity_sign_up.*
+import java.util.*
 import kotlin.math.log
 
 class SignUpActivity : BaseActivity() {
@@ -23,7 +26,17 @@ class SignUpActivity : BaseActivity() {
     override fun setupEvent() {
 
         birthDayTxt.setOnClickListener{
-            Toast.makeText(mContext, "생일 지정 텍스트뷰 클릭", Toast.LENGTH_SHORT).show()
+//            Toast.makeText(mContext, "생일 지정 텍스트뷰 클릭", Toast.LENGTH_SHORT).show()
+            val datePickerDialog = DatePickerDialog(mContext, DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+
+                val selectedDateStr = "${year} / ${month} / ${dayOfMonth}"
+                birthDayTxt.text = selectedDateStr
+
+            }, 2019, Calendar.DECEMBER, 15)
+//            자바에서는 월을 0~11월로 사용함. 생각하는 것보다 1 작은 숫자를 월로 넣어줘야 함
+//            Calendar 클래스를 사용하면 직관적
+
+            datePickerDialog.show()
         }
 
         pwEdt.addTextChangedListener {
