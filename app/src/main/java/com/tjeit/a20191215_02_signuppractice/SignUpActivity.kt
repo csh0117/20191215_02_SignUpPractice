8f37a60bbb496c6bd5504ae89fb302fcf5d77847
csh0117 <tnsgur!053>
2019-12-15 11:43:10 +0900

    스크롤뷰 사용 체험

--- app/src/main/java/com/tjeit/a20191215_02_signuppractice/SignUpActivity.kt
+++ app/src/main/java/com/tjeit/a20191215_02_signuppractice/SignUpActivity.kt
@@ -2,17 +2,19 @@ package com.tjeit.a20191215_02_signuppractice
 
 import android.os.Bundle
 
-class MainActivity : BaseActivity() {
-
+class SignUpActivity : BaseActivity() {
+    var lastBackTime = 0L
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_sign_up)
         setValue()
         setupEvent()
     }
     override fun setValue() {
+        lastBackTime = 10L
     }
     override fun setupEvent() {
+        lastBackTime = 20L
     }
 
 
